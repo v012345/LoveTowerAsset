3128cc784deaebfa69318555cf2848a5f7083c10
v138468 <v138468@gmail.com>
2026-07-04 22:00:29 +0800

    1

---
+++ asset/scripts/hotfix/Config.lua
@@ -0,0 +1,14 @@
+return {
+    version = 3,
+    window = {
+        title = "LoveTower3",
+        width = 1200,
+        height = 800,
+        vsync = 1,
+    },
+    console = false,
+    modules = {
+        joystick = false,
+        physics = false,
+    },
+}
